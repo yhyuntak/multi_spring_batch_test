617eba340c748466324f952739a7309d6c318090
yhyuntak <yhyuntak@gmail.com>
2024-09-09 14:11:28 +0900

    init commit

---
+++ src/main/java/com/sds/spring_batch_test/spring_multi_server/SpringMultiServerApplication.java
@@ -0,0 +1,13 @@
+package com.sds.spring_batch_test.spring_multi_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sds/spring_batch_test/spring_multi_server/SpringMultiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.spring_batch_test.spring_multi_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
