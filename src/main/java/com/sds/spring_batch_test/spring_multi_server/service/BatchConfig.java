2173a7e575502c4253e63eac4fb779783cd5529f
yhyuntak <yhyuntak@gmail.com>
2024-09-09 18:31:10 +0900

    init 2

---
+++ src/main/java/com/sds/spring_batch_test/spring_multi_server/service/BatchConfig.java
@@ -0,0 +1,12 @@
+package com.sds.spring_batch_test.spring_multi_server.service;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+}
